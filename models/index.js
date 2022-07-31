ea1f1bf7203883f87f9873537c4a1730ce5d7398
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-07-31 19:28:50 -0400

    create vote on posts by users

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
+// User posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -11,4 +13,33 @@ Post.belongsTo(User, {
     onDelete: 'cascade',
 });
 
-module.exports = { User, Post };
+// User votes
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
