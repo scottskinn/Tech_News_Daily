fc2047dbb5eb9271baac0b1ec5e17f546976716b
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-08-03 16:41:31 -0400

    create edit and delete post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,7 +5,7 @@ const withAuth = require('../utils/auth');
 
 const { Post, User, Comment } = require('../models');
 
-router.get('/', withAuth (req, res) => {
+router.get('/', withAuth, (req, res) => {
     Post.findAll({
       where: {
         // use the ID from the session
@@ -42,7 +42,56 @@ router.get('/', withAuth (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
+
+router.get('/edit/:id', (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({
+                message: 'No post found with this id'
+            });
+            return;
+        }
+
+        // serialize the data
+        const post = dbPostData.get({
+            plain: true
+        });
+
+        // pass data to template
+        res.render('edit-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
 
 
 
